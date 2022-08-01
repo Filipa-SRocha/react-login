d221a07b5839c728a75cb457a5906bc715c17ae9
Filipa-SRocha <filipa.sr2@gmail.com>
2022-08-01 13:48:02 +0100

    Mudar display de data

--- src/pages/people/Person.jsx
+++ src/pages/people/Person.jsx
@@ -1,10 +1,13 @@
 import { PersonInfoContainer } from './People.styled';
+import moment from 'moment';
 
 const Person = ({ person }) => {
+	const data = new Date(person.dataNascimento);
+
 	return (
 		<PersonInfoContainer>
 			<p>{person.nome}</p>
-			<p>{person.dataNascimento}</p>
+			<p>{moment(data).locale('pt').format('LL')}</p>
 			<p>{person.cpf}</p>
 			<p>{person.email}</p>
 		</PersonInfoContainer>
